372808eda461e05d58d7a9c745c94a08dfce9713
RizwanAmjad <rizwanamjadms@gmail.com>
2024-04-07 12:40:37 +0500

    Textual updates

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -178,17 +178,9 @@ function Home() {
                 <div className="flex items-center">
                   <GrHomeRounded />
                   <div className="flex flex-col items-start ms-4">
-                    <div>SI HER Talent Collective</div>
+                    <div>Si Her Site</div>
                     <div className="text-sm">Home</div>
                   </div>
-                </div>
-                <div>
-                  <button
-                    type="button"
-                    class="focus:outline-none text-white bg-[#DA486B] hover:opacity-70 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-1.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
-                  >
-                    Upgrade
-                  </button>
                 </div>
               </div>
               <div class="flex items-center justify-center w-full sm:ml-6">
@@ -451,7 +443,7 @@ function Home() {
                 type="search"
                 id="default-search"
                 class=" w-full p-2.5 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Assign domain to webpage"
+                placeholder="Submit your siher.eth domain of choice"
                 value={subdomain}
                 onChange={(e) => {
                   console.log(e.target.value)
