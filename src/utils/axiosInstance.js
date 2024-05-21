02373424bc6db5780d1c7e5b218959e04380c338
Muhammad Rafay <rafaymuhammad245@gmail.com>
2024-05-21 09:23:56 +0500

    jwt malfored bug fixed

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,11 +1,37 @@
-import axios from "axios";
+// import axios from "axios";
 
+// const data = JSON.parse(localStorage.getItem("SI_HER"));
+// console.log(data);
+// const axiosInstance = axios.create({
+//   baseURL: process.env.REACT_APP_BASE_URL,
+//   headers: {
+//     Authorization: `Bearer ${data?.token}`,
+//   },
+// });
+
+// export default axiosInstance;
+import axios from "axios";
 const data = JSON.parse(localStorage.getItem("SI_HER"));
+const token = data ? data.token : null;
+
 const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
-    Authorization: `Bearer ${data?.token}`,
+    Authorization: token ? `Bearer ${token}` : '',
   },
 });
+axiosInstance.interceptors.request.use(
+  config => {
+    const data = JSON.parse(localStorage.getItem("SI_HER"));
+    const token = data ? data.token : null;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
